99fbd8cd551d64accae8e004aa880910846a36e3
aviladiogo <aviladiogo30@gmail.com>
2022-11-14 17:51:03 -0300

    100%

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from linkedin_api import Linkedin
 import env
 import json
 import boto3
+import requests
 
-user = env.getUser()
-password = env.getPassword()
+token = env.getToken()
 
-api = Linkedin(user, password)
+def getData():
+    base_url = f'https://api.linkedin.com/v2/me?oauth2_access_token={token}'
+    request = requests.get(base_url)
+    return request.json()
 
-def getContact(target):
-    return api.get_profile_contact_info(target)
-
-def getProfile(target):
-    return api.get_profile(target)
+def getEmail():
+    base_url = f'https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))&oauth2_access_token={token}'
+    request = requests.get(base_url)
+    return request.json()
 
 def sendToDynamoDB(event):
     client_dynamo=boto3.resource('dynamodb')
@@ -22,19 +23,12 @@ def sendToDynamoDB(event):
         return "Done"
     except:
         raise
+
 def lambda_handler(event, context):
-    arq = open("usuarios.txt") # open file with users to fetch
-    linhas = arq.readlines()
-    for linha in linhas:
-        for target in linha.split(', '):
-            data = {}
-            print(f'searching contact info from {target}...')
+    data = {}
+    data['ID'] = getData()['id']
+    data['Name'] = getData()['localizedFirstName']
+    data['LastName'] = getData()['localizedLastName']
+    data['Email'] = getEmail()['elements'][0]['handle~']['emailAddress']
 
-            data['Email'] = getContact(target)["email_address"]
-            data['FirstName'] = getProfile(target)["firstName"]
-            data['LastName'] = getProfile(target)["lastName"]
-            data['HeadLine'] = getProfile(target)["headline"]
-            data['University'] = getProfile(target)["education"][0]["school"]["schoolName"]
-
-            data = json.dumps(data, ensure_ascii=False).encode('utf8') # converting dictoinary to json and encoding to utf8 
-            sendToDynamoDB(data.decode()) # decoding to utf8 and sending to dynamoDB
+    sendToDynamoDB(data) # decoding to utf8 and sending to dynamoDB
